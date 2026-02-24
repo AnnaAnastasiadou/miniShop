cf06ec63d415dd76404e72bc464e7321f9271ed1
Anna <anna@Annas-MacBook-Pro.local>
2026-02-24 11:15:00 +0200

    categories and products loaded in parallel

--- app/src/main/java/com/example/minishop/feature/products/home/HomeViewModel.kt
+++ app/src/main/java/com/example/minishop/feature/products/home/HomeViewModel.kt
@@ -4,10 +4,13 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.minishop.data.mapper.toProduct
 import com.example.minishop.data.remote.NetworkResult
+import com.example.minishop.data.remote.category.CategoryDto
+import com.example.minishop.data.remote.products.ProductDto
 import com.example.minishop.data.repository.category.CategoryRepository
 import com.example.minishop.data.repository.products.ProductsRepository
 import com.example.minishop.feature.Product
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.async
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
@@ -27,91 +30,103 @@ class HomeViewModel @Inject constructor(
     private var currentCategoryProducts: List<Product>? = null
 
     init {
-        loadCategories()
-        loadProducts()
+        loadData()
     }
 
-    fun loadCategories() {
-        viewModelScope.launch {
-            _uiState.update {
-                it.copy(
-                    categoriesUiState = it.categoriesUiState.copy(
-                        isLoading = true,
-                        error = null,
-                        data = null
-                    )
-                )
-            }
-            when (val response = categoryRepository.getCategories()) {
-                is NetworkResult.Success -> {
-                    val categories = listOf("all") + response.data
-                    _uiState.update {
-                        it.copy(
-                            categoriesUiState = it.categoriesUiState.copy(
-                                isLoading = false,
-                                data = categories,
-                                error = null
-                            )
-                        )
-                    }
-                }
 
-                is NetworkResult.Error -> {
-                    _uiState.update {
-                        it.copy(
-                            categoriesUiState = it.categoriesUiState.copy(
-                                isLoading = false,
-                                data = null,
-                                error = response.message
-                            )
+    fun loadData() {
+        viewModelScope.launch {
+            val categoriesAsync = async { categoryRepository.getCategories() }
+            val productAsync = async { productsRepository.getAllProducts() }
+
+            val categoriesRes = categoriesAsync.await()
+            val productRes = productAsync.await()
+
+
+            loadCategories(categoriesRes)
+            loadProducts(productRes)
+        }
+    }
+
+    private fun loadCategories(categoryRes: NetworkResult<List<String>>) {
+        _uiState.update {
+            it.copy(
+                categoriesUiState = it.categoriesUiState.copy(
+                    isLoading = true,
+                    error = null,
+                    data = null
+                )
+            )
+        }
+        when (categoryRes) {
+            is NetworkResult.Success -> {
+                val categories = listOf("all") + categoryRes.data
+                _uiState.update {
+                    it.copy(
+                        categoriesUiState = it.categoriesUiState.copy(
+                            isLoading = false,
+                            data = categories,
+                            error = null
                         )
-                    }
+                    )
+                }
+            }
+
+            is NetworkResult.Error -> {
+                _uiState.update {
+                    it.copy(
+                        categoriesUiState = it.categoriesUiState.copy(
+                            isLoading = false,
+                            data = null,
+                            error = categoryRes.message
+                        )
+                    )
                 }
             }
         }
     }
 
-    fun loadProducts() {
-        viewModelScope.launch {
-            _uiState.update {
-                it.copy(
-                    productsUiState = it.productsUiState.copy(
-                        isLoading = true,
-                        data = null,
-                        error = null
-                    )
-                )
-            }
-            when (val response = productsRepository.getAllProducts()) {
-                is NetworkResult.Success -> {
-                    val products = response.data.map { it.toProduct() }
-                    groupedProducts = products.groupBy { it.category.lowercase() }
-                    _uiState.update {
-                        it.copy(
-                            productsUiState = it.productsUiState.copy(
-                                isLoading = false,
-                                data = products,
-                                error = null
-                            )
-                        )
-                    }
-                    allProducts = products
-                    currentCategoryProducts = products
-                }
 
-                is NetworkResult.Error -> {
-                    _uiState.update {
-                        it.copy(
-                            productsUiState = it.productsUiState.copy(
-                                isLoading = false,
-                                data = null,
-                                error = response.message
-                            )
+    private fun loadProducts(productsRes: NetworkResult<List<ProductDto>>) {
+        _uiState.update {
+            it.copy(
+                productsUiState = it.productsUiState.copy(
+                    isLoading = true,
+                    data = null,
+                    error = null
+                )
+            )
+        }
+        when (productsRes) {
+            is NetworkResult.Success -> {
+                val products = productsRes.data.map { it.toProduct() }
+                groupedProducts = products.groupBy { it.category.lowercase() }
+                _uiState.update {
+                    it.copy(
+                        productsUiState = it.productsUiState.copy(
+                            isLoading = false,
+                            data = products,
+                            error = null
                         )
-                    }
+                    )
+                }
+                allProducts = products
+                currentCategoryProducts = products
+            }
+
+            is NetworkResult.Error -> {
+                _uiState.update {
+                    it.copy(
+                        productsUiState = it.productsUiState.copy(
+                            isLoading = false,
+                            data = null,
+                            error = productsRes.message
+                        )
+                    )
                 }
             }
         }
+
     }
 
     fun onEvent(event: HomeScreenUiEvent) {
@@ -128,8 +143,10 @@ class HomeViewModel @Inject constructor(
             }
 
             is HomeScreenUiEvent.OnSearch -> {
-                uiState.value.productsUiState.data?.let{ data ->
-                    val searchedProducts = currentCategoryProducts?.filter { it.title.lowercase().contains(event.query.lowercase()) }
+                uiState.value.productsUiState.data?.let { data ->
+                    val searchedProducts = currentCategoryProducts?.filter {
+                        it.title.lowercase().contains(event.query.lowercase())
+                    }
                     _uiState.update { it.copy(productsUiState = it.productsUiState.copy(data = searchedProducts)) }
                 }
             }
