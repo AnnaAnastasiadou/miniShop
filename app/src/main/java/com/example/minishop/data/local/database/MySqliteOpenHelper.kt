24870544c098b3c0c15c69622e2194831a193d36
Anna <anna@Annas-MacBook-Pro.local>
2026-02-25 16:53:34 +0200

    Create the tables only onCreate, version 1

--- app/src/main/java/com/example/minishop/data/local/database/MySqliteOpenHelper.kt
+++ app/src/main/java/com/example/minishop/data/local/database/MySqliteOpenHelper.kt
@@ -8,14 +8,13 @@ class MySqliteOpenHelper(private val context: Context) :
     SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
     companion object {
         private const val DATABASE_NAME = "Products.db"
-        private const val DATABASE_VERSION = 2
+        private const val DATABASE_VERSION = 1
     }
 
     override fun onCreate(db: SQLiteDatabase?) {
         db?.execSQL(Tables.Favorites.CREATE_TABLE)
-    }
-
-    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
         db?.execSQL(Tables.Cart.CREATE_TABLE)
     }
+
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {}
 }
