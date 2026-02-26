1e78c05aa66b81c4bff6ca49e1358cf1b13356d9
Anna <anna@Annas-MacBook-Pro.local>
2026-02-26 14:35:28 +0200

    Use stateIn with WhileSubscribed for Favorite data load

--- app/src/main/java/com/example/minishop/feature/products/favorites/FavoriteProductsViewModel.kt
+++ app/src/main/java/com/example/minishop/feature/products/favorites/FavoriteProductsViewModel.kt
@@ -6,8 +6,13 @@ import com.example.minishop.data.local.datasource.FavoriteProductsDatasource
 import com.example.minishop.data.repository.favorites.FavoritesRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -15,19 +20,14 @@ import javax.inject.Inject
 @HiltViewModel
 class FavoriteProductsViewModel @Inject constructor(
     private val favoritesRepository: FavoritesRepository
-): ViewModel() {
-    private val _uiState = MutableStateFlow(FavoriteProductsUiState())
-    val uiState = _uiState.asStateFlow()
-
-    init {
-        viewModelScope.launch{
-            favoritesRepository.loadFavorites()
-        }
-        viewModelScope.launch{
-            favoritesRepository.favoriteProducts().collect{updatedList ->
-                _uiState.update { it.copy(data = updatedList) }
-            }
-        }
-    }
+) : ViewModel() {
+    val uiState = favoritesRepository.favoriteProducts()
+        .onStart { favoritesRepository.loadFavorites() }
+        .map { products -> FavoriteProductsUiState(data = products) }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(500L),
+            initialValue = FavoriteProductsUiState()
+        )
 
 }
